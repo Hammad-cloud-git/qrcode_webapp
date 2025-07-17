65d0a614b35e950491d71bbed67c4dbdf0384580
Hammad_07 <hammadkhalid1110@outlook.com>
2025-07-17 13:25:33 +0500

    code edit

--- qr_app.py
+++ qr_app.py
@@ -8,5 +8,5 @@ data = st.text_input("Enter the any data (number, text, link, etc.): ")
 if st.button("Generate") and data:
     img = qrcode.make(data)
     img.save("QR Code_app.png")
-    st.image("QR Code_app,png")
+    st.image("QR Code_app.png")
     st.success("The QR Code has been generated Successfully! ")
